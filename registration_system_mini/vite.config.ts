a67df88af975995c4f6580cb4fc34d3539441319
WangRui <wangrui@betalpha.com>
2026-08-10 13:03:10 +0800

    build(mini): support subpath H5 deployment

--- registration_system_mini/vite.config.ts
+++ registration_system_mini/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import uni from "@dcloudio/vite-plugin-uni";
 
 export default defineConfig({
+  base: process.env.VITE_PUBLIC_BASE || "/",
   plugins: [uni()],
 });
