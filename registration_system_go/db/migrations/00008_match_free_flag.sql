468944816e73aae88e0b8c1406de7946162cdc57
WangRui <wangrui@betalpha.com>
2026-08-17 10:01:55 +0800

    fix(go): annotate 00008 with goose Up/Down directives

--- registration_system_go/db/migrations/00008_match_free_flag.sql
+++ registration_system_go/db/migrations/00008_match_free_flag.sql
@@ -1,4 +1,8 @@
+-- +goose Up
 -- 比赛是否免费报名：新建比赛默认免费；
 -- 历史比赛（legacy 迁移）均为收费结算，回填为不免费。
 ALTER TABLE matches ADD COLUMN is_free BOOLEAN NOT NULL DEFAULT TRUE;
 UPDATE matches SET is_free = FALSE;
+
+-- +goose Down
+ALTER TABLE matches DROP COLUMN is_free;
