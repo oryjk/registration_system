b8f01817d6526b0ab1e08869d45abb0f21166cf4
WangRui <wangrui@betalpha.com>
2026-08-25 00:49:48 +0800

    chore(mini): allocate build version 1.0.50 via mini-review registry

--- registration_system_mini/src/config/generatedMiniProgramVersion.ts
+++ registration_system_mini/src/config/generatedMiniProgramVersion.ts
@@ -1,2 +1,2 @@
-export const MINI_PROGRAM_VERSION = "1.0.49";
-export const MINI_PROGRAM_VERSION_CODE = "10049";
+export const MINI_PROGRAM_VERSION = "1.0.50";
+export const MINI_PROGRAM_VERSION_CODE = "10050";
