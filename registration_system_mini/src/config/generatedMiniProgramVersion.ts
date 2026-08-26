a43ffcd05fee29ba9b8e45e6a14c334fdab99e3d
WangRui <wangrui@betalpha.com>
2026-08-26 11:57:32 +0800

    chore(mini): bump version to 1.0.52

--- registration_system_mini/src/config/generatedMiniProgramVersion.ts
+++ registration_system_mini/src/config/generatedMiniProgramVersion.ts
@@ -1,2 +1,2 @@
-export const MINI_PROGRAM_VERSION = "1.0.51";
-export const MINI_PROGRAM_VERSION_CODE = "10051";
+export const MINI_PROGRAM_VERSION = "1.0.52";
+export const MINI_PROGRAM_VERSION_CODE = "10052";
