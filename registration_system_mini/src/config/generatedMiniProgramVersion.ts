3521dcfb8e89d276e17e1f212fd280b45e8340dc
WangRui <wangrui@betalpha.com>
2026-09-25 15:59:00 +0800

    chore(mini): record build version 1.0.73

--- registration_system_mini/src/config/generatedMiniProgramVersion.ts
+++ registration_system_mini/src/config/generatedMiniProgramVersion.ts
@@ -1,2 +1,2 @@
-export const MINI_PROGRAM_VERSION = "1.0.72";
-export const MINI_PROGRAM_VERSION_CODE = "10072";
+export const MINI_PROGRAM_VERSION = "1.0.73";
+export const MINI_PROGRAM_VERSION_CODE = "10073";
